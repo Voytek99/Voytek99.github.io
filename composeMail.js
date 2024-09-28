9d7c24fa80a4fe9aa07125ce55e346b7725f3101
WojciechMierzwa <wojciech.piotr.mierzwa@gmail.com>
2024-09-28 13:47:56 +0200

    Update composeMail.js

--- composeMail.js
+++ composeMail.js
@@ -2,7 +2,7 @@ let ticketArea = "";
 
 function copyText(text) {
     navigator.clipboard.writeText(text).then(() => {
-        console.log('Copied successfully!');
+       
     }).catch(err => {
         console.error('Failed to copy: ', err);
     });
@@ -21,10 +21,10 @@ function printRows(commands, element) {
         });
 
         container.appendChild(button);
+
         element.appendChild(container);
     });
 }
-
 function extractInfoBetween(ticketArea, startKeyword, endKeyword) {
     const regex = new RegExp(`${startKeyword}:?\\s*([\\s\\S]*?)${endKeyword}:?`, 'i');
     const match = ticketArea.match(regex);
@@ -34,30 +34,33 @@ function extractInfoBetween(ticketArea, startKeyword, endKeyword) {
     return (extractedValue.length > 50 || extractedValue === "") ? "-" : extractedValue;
 }
 
-function classify() {
+
+
+
+function classify(){
     ticketArea = document.getElementById('inputTicket').value;
     
     const commandList = document.getElementById('commandList');
     commandList.innerHTML = '';
     const basicCommand = document.getElementById('basicCommand');
     basicCommand.innerHTML = '';
+    const hostname = extractInfoBetween(ticketArea, "Host Name", "Last Ticket Number");
+    const serialNumber = extractInfoBetween(ticketArea, "Serial number", "Asset tag");
+    const modelID = extractInfoBetween(ticketArea, "Model ID", "Manufacturer");
 
-    // Extracting the values between marked "here" and relevant keywords
+    
     const specialLocationInfo = extractInfoBetween(ticketArea, "Special Location Info", "Location");
-    const location = extractInfoBetween(ticketArea, "Location", "Mounting Height U");
     const building = extractInfoBetween(ticketArea, "Building", "Floor");
     const floor = extractInfoBetween(ticketArea, "Floor", "Room");
-    const room = extractInfoBetween(ticketArea, "Room", "Mngd Building");
+    const room  = extractInfoBetween(ticketArea, "Room", "Mngd Building");
 
-    // Prepare the strings to display
-    let locationDetails = `Special Location Info: ${specialLocationInfo}\nLocation: ${location}\nBuilding: ${building}\nFloor: ${floor}\nRoom: ${room}`;
 
-    // Store data in an array and print it in the UI
-    let data = [locationDetails];
-    printRows(data, commandList);
+    let specs = "Name: "+hostname +"\nS/N: " + serialNumber + "\nModel ID: " + modelID;
+
+    let lcon_location = "Special Location Info: " + specialLocationInfo +  "\nLocation: " + "\nBuilding: " + building +  "\nFloor: " + floor + "\nRoom: " + room;
+    
+    data = [specs, lcon_location];
+    printRows(data,commandList);
+
 }
 
-function removeFirstOccurrence(text, keyword) {
-    const regex = new RegExp(keyword, 'i');  // Case-insensitive search for the keyword
-    return text.replace(regex, '');  // Replace only the first occurrence
-}
